5ac77341068b6db869f81d88533cc01775ae69ec
TheanoSpiliopoulou <158092094+TheanoSpiliopoulou@users.noreply.github.com>
2025-11-13 12:39:00 +0200

    Update sentiment_train.py

--- sentiment_train.py
+++ sentiment_train.py
@@ -38,11 +38,33 @@ class DataSetChecker:
     - Είναι οι ετικέτες μέσα στο φάσμα που εχω ορίει; (0,1,2);
     """
 
-    def __init__(self, path: str, valid_labels=(0, 1, 2)):
-        self.path = path
+    def __init__(self,csv_path):
+        self.csv_path = csv_path
       #με αυτόν τον τρόπο αποθηκεύω τη διαδρoμή του αρχείου που θα ελέγξω
-        self.valid_labels = set(valid_labels)
-        self.df = None 
-      #Ουσιαστικά είναι η θέση που θα μπει ο πίνακας αφού διαβαστεί 
+    def file_exists(self)
+      print("Βήμα 1: Έλεγχος ύπαρξης αρχεόυ.Αν το αρχείο υπάρχε επιστρέφει True ενώ σε δαφορετική περίπτωση False")
+      return
+      os.path.isfile(sewlf.csv_path)
+    def load_csv(self):
+        print("Βήμα 2: Φόρτωση του CSV σύμφωνα με την κωδκοποίηση UTF-8.")
+      #Η pandas.read_csv φορτώνει το αρχεό σε ένα Data Frame
+        return pd.read_csv(self.csv_path, encoding="utf-8")
 
-     
+    def check_columns(self, df):
+      #ελέγχω αν υπάρχουν ο στήλες 'text','labels' που θέλω
+       print("Βήμα 3: Έλεγχος σωστώ στηλών του π΄νακα)
+            # Δίνω το σύνολο των στηλών που χρειάζονται
+        required = {"text", "label"}
+        #Υπολογίζω ποιες στήλες λείπουν από τον πίνακα
+        missing = required - set(df.columns)
+        #Δίνει το σύνολο με τς λέξεις που λείπουν σε διαφορετική περ΄πτωση που δεν λείπει καμία το σύνολο είνα κενό)
+        return missing
+
+    def check_missing_values(self, df):
+    #Ελέγχει την ύπαρξη κενών τιμών
+        print("Βήμα 4: Έλεγχος για κενές τιμέσ σε text και label.")
+        missing_text = df["text"].isna().sum()
+        missing_label = df["label"].isna().sum()
+        return missing_text, missing_label
+
+    
